82f7d584d4f391e8c5cd9a56fbf4b961ce88f274
Anthony Nepomuceno <anepom3@illinois.edu>
2019-10-04 08:34:44 -0500

    "Initializing iOSHealthMirror Files"

---
+++ iOSHealthMirror/iOSHealthMirror/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  iOSHealthMirror
+//
+//  Created by Nepomuceno, Anthony P on 10/4/19.
+//  Copyright © 2019 Anthony Nepomuceno. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello World")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
